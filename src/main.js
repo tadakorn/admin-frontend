592c51780246f04134db7a9394db9ef079d83e7f
tadakorn <tadakorn.nakkaew@gmail.com>
2023-06-13 14:55:14 +0700

    add nav and sidebar

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import './assets/main.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 
 import { createApp } from 'vue'
@@ -8,9 +8,28 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faBarsStaggered, faTerminal } from '@fortawesome/free-solid-svg-icons'
+import {
+  faUser,
+  faHeart,
+  faComments,
+  faFolderClosed,
+  faMessage
+} from '@fortawesome/free-regular-svg-icons'
+
+/* add icons to the library */
+library.add(faBarsStaggered, faUser, faHeart, faTerminal, faComments, faFolderClosed, faMessage)
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi, quam esse itaque, ipsam omnis
+      officiis dicta quisquam maiores atque voluptatum voluptate, reiciendis dolorum. Pariatur quas
+      aspernatur ipsum quam! Architecto, sapiente?
+    </h1>
   </main>
 </template>
