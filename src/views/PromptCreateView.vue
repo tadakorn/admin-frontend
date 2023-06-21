2a0b85ae328fa941bf834f60afe0ca287f2e2dbd
tadakorn <tadakorn.nakkaew@gmail.com>
2023-06-21 22:00:37 +0700

    (:lipstick:)update page prompt and system create

--- src/views/PromptCreateView.vue
+++ src/views/PromptCreateView.vue
@@ -17,32 +17,32 @@ function create() {
 
 <template>
   <div class="container">
-    <div class="h5 pt-4">Create Prompt</div>
+    <div class="h5">Create Prompt</div>
 
-    <table class="table">
-      <tbody>
-        <tr>
-          <td scope="col">title</td>
-          <td>
-            <input type="text" class="form-control" v-model="prompt.title" />
-          </td>
-        </tr>
-        <tr>
-          <td scope="col">detail</td>
-          <td>
-            <input type="text" class="form-control" v-model="prompt.detail" />
-          </td>
-        </tr>
-        <tr>
-          <td scope="col">prompt</td>
-          <td>
-            <input type="text" class="form-control" v-model="prompt.prompt" />
-          </td>
-        </tr>
-      </tbody>
-    </table>
-    <div class="d-flex justify-content-end">
-      <button type="button" class="btn btn-success btn-sm" @click="create">Create</button>
+    <div class="bg-white p-4 rounded-3">
+      <div class="row align-items-center">
+        <div class="col-12 col-md-2 title">title</div>
+        <div class="col-12 col-md-6">
+          <input type="text" class="form-control" v-model="prompt.title" />
+        </div>
+      </div>
+
+      <div class="row align-items-center mt-4">
+        <div class="col-12 col-md-2 title">detail</div>
+        <div class="col-12 col-md-6">
+          <input type="text" class="form-control" v-model="prompt.detail" />
+        </div>
+      </div>
+
+      <div class="row align-items-center mt-4">
+        <div class="col-12 col-md-2 title">prompt</div>
+        <div class="col-12 col-md-6">
+          <input type="text" class="form-control" v-model="prompt.prompt" />
+        </div>
+      </div>
+    </div>
+    <div class="d-flex justify-content-end my-4">
+      <button type="button" class="btn btn-success" @click="create">Create</button>
     </div>
   </div>
 </template>
@@ -50,5 +50,13 @@ function create() {
 <style scoped>
 td {
   border: none;
+}
+.title {
+  font-weight: 700;
+}
+@media (max-width: 600px) {
+  .title {
+    margin-bottom: 10px;
+  }
 }
 </style>
--- src/views/SystemPersonaCreateView.vue
+++ src/views/SystemPersonaCreateView.vue
@@ -44,13 +44,13 @@ async function create() {
 
     <div class="bg-white p-4 rounded-3">
       <div class="row align-items-center">
-        <div class="col-12 col-md-2">name</div>
+        <div class="col-12 col-md-2 title">name</div>
         <div class="col-12 col-md-6">
           <input type="text" class="form-control" v-model="systemPersona.name" />
         </div>
       </div>
       <div class="row align-items-center mt-4">
-        <div class="col-12 col-md-2">image</div>
+        <div class="col-12 col-md-2 title">image</div>
         <div class="col-12 col-md-6">
           <div class="row">
             <div class="col-12 col-md-6">
@@ -62,20 +62,24 @@ async function create() {
             </div>
             <div class="col-12">
               <label for="formFileDisabled" class="form-label"></label>
-              <input class="form-control" type="file" id="formFileDisabled" v-on:change="onImageChange" />
+              <input
+                class="form-control"
+                type="file"
+                id="formFileDisabled"
+                v-on:change="onImageChange"
+              />
             </div>
           </div>
-
         </div>
       </div>
       <div class="row align-items-center mt-4">
-        <div class="col-12 col-md-2">prompt</div>
+        <div class="col-12 col-md-2 title">prompt</div>
         <div class="col-12 col-md-6">
           <textarea class="form-control" v-model="systemPersona.prompt" rows="3"></textarea>
         </div>
       </div>
     </div>
-      
+
     <div class="d-flex justify-content-end my-4">
       <button type="button" class="btn btn-success" @click="create">Create</button>
     </div>
@@ -85,5 +89,13 @@ async function create() {
 <style scoped>
 td {
   border: none;
+}
+.title {
+  font-weight: 700;
+}
+@media (max-width: 600px) {
+  .title {
+    margin-bottom: 10px;
+  }
 }
 </style>
